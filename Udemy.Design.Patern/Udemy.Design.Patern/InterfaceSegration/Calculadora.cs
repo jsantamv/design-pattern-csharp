9d0b78ea9152a08290c43588ca78276502fd781d
Juan Carlos Santamaría Villegas <jsantamv@gmail.com>
2022-12-27 14:43:46 -0600

    Segregacion de Interfaz

---
+++ Udemy.Design.Patern/Udemy.Design.Patern/InterfaceSegration/Calculadora.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Udemy.Design.Patern.InterfaceSegration.Interface;
+
+namespace Udemy.Design.Patern.InterfaceSegration
+{
+
+    /// <summary>
+    /// En este ejemplo, hemos creado una interfaz llamada IOperaciones que define dos métodos públicos: 
+    /// Sumar y Restar. Luego, hemos creado una clase llamada Calculadora que implementa esta interfaz. 
+    /// Esto significa que la clase Calculadora debe proporcionar implementaciones para ambos métodos.
+    /// Hemos utilizado una propiedad privada llamada _resultado para almacenar el resultado de las 
+    /// operaciones de suma y resta. Los clientes de la clase Calculadora pueden usar la interfaz IOperaciones
+    /// para llamar a estos métodos públicos sin tener que conocer los detalles de la implementación de la clase.
+    /// </summary>
+    public class Calculadora : IOperaciones
+    {
+        private int _resultado;
+
+        public int Sumar(int a, int b)
+        {
+            _resultado = a + b; return _resultado;
+        }
+
+        public int Restar(int a, int b)
+        {
+            _resultado = a - b; return _resultado;
+        }
+    }
+}
--- Udemy.Design.Patern/Udemy.Design.Patern/Program.cs
+++ Udemy.Design.Patern/Udemy.Design.Patern/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Xml.Linq;
+using Udemy.Design.Patern.InterfaceSegration;
+using Udemy.Design.Patern.InterfaceSegration.Interface;
 using Udemy.Design.Patern.Liskov;
 using Udemy.Design.Patern.Liskov.Abstract;
 using Udemy.Design.Patern.Liskov.Interface;
@@ -8,35 +10,36 @@ using Udemy.Design.Patern.Strategy;
 
 
 #region Strategy Pattern
-//Console.WriteLine("=====< Strategy Pattern >======");
-//Console.WriteLine();
-//Contexto context = new Contexto(new AntivirusSimple());
-//context.Ejecutar();
+Console.WriteLine("=====< Strategy Pattern >======");
+Console.WriteLine();
+Contexto context = new(new AntivirusSimple());
+context.Ejecutar();
 
-//Console.WriteLine();
+Console.WriteLine();
 
-//Contexto contextA = new Contexto(new AntivirusAvanzado());
-//contextA.Ejecutar();
+Contexto contextA = new(new AntivirusAvanzado());
+contextA.Ejecutar();
 #endregion
 
 #region Open Closed Principle
 Console.WriteLine();
 Console.WriteLine("======< Open Closed Principle >======");
 Invoice Invoice = new();
-IList<Water> lstDrink = new List<Water>();
-
-lstDrink.Add(new Water
+IList<Water> lstDrink = new List<Water>
 {
-    Name = "alpina",
-    Invoice = 0.20m,
-    Price = 10
-});
-lstDrink.Add(new Water
-{
-    Name = "alpina",
-    Invoice = 0.20m,
-    Price = 1012
-});
+    new Water
+    {
+        Name = "alpina",
+        Invoice = 0.20m,
+        Price = 10
+    },
+    new Water
+    {
+        Name = "alpina",
+        Invoice = 0.20m,
+        Price = 1012
+    }
+};
 
 var result = Invoice.GetTotal(lstDrink);
 
@@ -57,5 +60,22 @@ Fruit fruit = new Orange();
 Console.WriteLine($"Orange: {fruit.GetColor()}");
 fruit = new Apple();
 Console.WriteLine($"Apple: {fruit.GetColor()}");
+
+#endregion
+
+
+#region Segregacion de Interface
+Console.WriteLine();
+Console.WriteLine("======< Segregacion de Interface >======");
+
+IOperaciones op = new Calculadora();
+double suma = op.Sumar(23, 12);
+
+Console.WriteLine($"Resultado de la suma: {suma}");
+
+double resta = op.Restar(23, 12);
+
+Console.WriteLine($"Resultado de la resta: {resta}");
+
 
 #endregion
