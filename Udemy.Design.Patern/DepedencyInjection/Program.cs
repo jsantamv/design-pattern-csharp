7a9fa2ce7f9214153386c0ae5ace316030f08dac
Juan Carlos Santamaría Villegas <jsantamv@gmail.com>
2023-01-02 08:27:04 -0600

    update

--- Udemy.Design.Patern/DepedencyInjection/Program.cs
+++ Udemy.Design.Patern/DepedencyInjection/Program.cs
@@ -1,8 +1,19 @@
 ﻿
 using DepedencyInjection;
 using DepedencyInjection.Interface;
+using Microsoft.Extensions.DependencyInjection;
 
-IDataAccess dal = new DataAccess();
+
+//Para utilizar esta libreria se instala este nuget
+//Microsoft.Extensions.DependencyInjection
+//Collecion de instancias.
+var collection = new ServiceCollection();
+collection.AddScoped<IDataAccess, DataAccess>();
+
+// 1. Primero creamos el Service provider.
+// para controlar la iyection
+var provider = collection.BuildServiceProvider();
+IDataAccess dal =  provider new DataAccess();
 IBusiness bix = new Business(dal);
 
 var userInterface = new UserInterface(bix);
